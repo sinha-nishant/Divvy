cca7d5ba984f1453cabb5ec13c659e807a037f8d
Nishant Sinha <nishant813@gmail.com>
2020-03-14 10:38:01 -0700

    Amended Twilio with more port info

--- Twilio.py
+++ Twilio.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
@@ -96,4 +98,5 @@ def sms():
     return str(resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
