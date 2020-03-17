2d473b845918330fb3ea21449890ab1a98a15e85
Nishant Sinha <nishant813@gmail.com>
2020-03-16 17:45:40 -0700

    Cleaned up Twilio and decreased processing time for sheets from ~8.5 seconds to ~1.5 seconds

--- Sheets.py
+++ Sheets.py
@@ -1,127 +1,107 @@
 import gspread, os, json
 from oauth2client.service_account import ServiceAccountCredentials
-from pprint import pprint
 from Order import *
-from googleapiclient import discovery
+from typing import Dict
 
 class Sheets:
     def __init__(self):
         # this specifies the scope for the actual google sheet
         self._scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
-                 "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
-        #get authentification for working with the google sheet
-        self._creds= ServiceAccountCredentials.from_json_keyfile_dict(json.loads(os.environ.get("SHEET_CREDS")),self._scope)
-        #checking if we have correct authentification
-        self._client : gspread.Client = gspread.authorize(self._creds)
+                       "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+        # get authentication for working with the google sheet
+        self._creds = ServiceAccountCredentials.from_json_keyfile_dict(json.loads(os.environ.get("SHEET_CREDS")), self._scope)
+        # checking if we have correct authentication
+        self._client: gspread.Client = gspread.authorize(self._creds)
 
-        self._test= None
+        self._order_record = self._client.open_by_key("1uRxor97NVAKBnljaOHr8SiJJfzN2UzQpUK8nkOTIQ5o").get_worksheet(0)
+        self._balances = self._client.open_by_key("1uRxor97NVAKBnljaOHr8SiJJfzN2UzQpUK8nkOTIQ5o").get_worksheet(1)
 
-        self._sheet = self._client.open("Doordash_Records").sheet1
-        self._sh = self._client.open("Doordash_Records")
-
-        #storing the data within the sheet
-        self._data = self._sheet.get_all_records()
-
-
-    def add(self,order):
-
+    def add(self, new_order):
+        # storing the order
+        order = new_order
         # used to store value of updated balances after incorporating new order
-        updated_balance : dict[str : float]= {}
-        if(order == None ):
+        updated_balance: dict[str: float] = {}
+        if not order:
             print('order doesnt exist')
-            return
-        # output is going to store the row which we are going to append to the sheet
-        output : list=[]
-        #stores the date of transaction
-        date : str = str(order.getDate().date()).replace("-", "/")
-        output.append(date)
-        #Stores restaraunt name
-        restaurant : str = order.getRestaurant()
-        output.append(restaurant)
-        #gets all the people who ordered and money spent
-        people : list[Member] = order.getMembers()
+        else:
+            # output is going to store the row which we are going to append to the sheet
+            output: list = []
+            # stores the date of transaction
+            date : str = str(order.getDate().date()).replace("-", "/")
+            output.append(date)
+            # Stores restaurant name
+            restaurant: str = order.getRestaurant()
+            output.append(restaurant)
+            # gets all the people who ordered and money spent
+            members: List[Member] = order.getMembers()
 
-        #adding value of total
-        output.append(order.getTotal())
+            # adding value of total
+            output.append(order.getTotal())
 
-        #this is going to be used to get all names in the sheet by retrieving the header row in the sheet
-        headings : list[str] = self._sheet.row_values(1)[3:]
+            # this is going to be used to get all names in the sheet by retrieving the header row in the sheet
+            headings: List[str] = self._order_record.row_values(1)[3:]
 
-        # this is a map from member name to amount spent
-        person2Total : dict[str : float] = {}
-        #initializing values per person to 0
-        for name in headings:
-            person2Total[name] = 0
+            memberToTotal = dict()
+            for member in members:
+                memberToTotal[member.getName()] = member.getTotal()
 
-        for person in people:
-            name : str = person.getName()
-            #error checking
-            if name not in person2Total:
-                # getting total
-                person2Total[name]: str= person.getTotal()
-                #adding name to the sheet
-                self._sheet.update_cell(1, 3+ len(person2Total),name)
-                self._sh.worksheets()[1].update_cell(1,len(person2Total), name)
-                self._sh.worksheets()[1].update_cell(2, len(person2Total), 0)
+            new_member_names = []
+            for key in memberToTotal:
+                if key not in headings:
+                    new_member_names.append(key)
 
+            if new_member_names:
+                new_member_range : List[gspread.Cell] = self._order_record.range(1, 3 + len(headings) + 1, 1, 3 + len(headings) + len(new_member_names))
+                for i, cell in enumerate(new_member_range):
+                    cell.value = new_member_names[i]
+                self._order_record.update_cells(new_member_range)
 
-            else:
-                #adding the value spent by the person to map
-                person2Total[name] : str =person.getTotal()
+            headings.extend(new_member_names)
+            for heading in headings:
+                if heading in memberToTotal.keys():
+                    output.append(memberToTotal[heading])
+                else:
+                    output.append(0)
 
-        #getting only the values of amount spent per person
+            self._order_record.insert_row(output, 2, value_input_option = 'USER_ENTERED')
 
-        person2Total_values : list[float]= person2Total.values()
+            # Now need to update balances
+            if new_member_names:
+                new_member_range: List[gspread.Cell] = self._balances.range(1, len(headings) - len(new_member_names) + 1, 1, len(headings))
+                for i, cell in enumerate(new_member_range):
+                    cell.value = new_member_names[i]
+                self._balances.update_cells(new_member_range)
 
-        #putting totals per person in output
-        index=1
-        headings = self._sheet.row_values(1)[3:]
-        for heading in headings:
-            inside= False
-            if heading not in person2Total:
-                output.append(0)
-            else:
-                output.append(person2Total[heading])
-                inside= True
-            if inside:
-                myCell = self._sh.worksheets()[1].find(heading)
-                original_balance= float(self._sh.worksheets()[1].cell(myCell.row+1,myCell.col).value)
-                self._sh.worksheets()[1].update_cell(myCell.row+1,myCell.col, original_balance + person2Total[heading])
-            index+=1
+            balance_range : List[gspread.Cell] = self._balances.range(2, 1, 2, len(headings))
+            for balance in balance_range:
+                if not balance.value:
+                    balance.value = 0
 
+            balance_dict : Dict[str : gspread.Cell] = dict()
+            updated_cells : List[gspread.Cell] = list()
+            for i, heading in enumerate(headings):
+                balance_dict[heading] = balance_range[i]
 
-        #adding to the sheet
-        self._sheet.insert_row(output,2, value_input_option = 'USER_ENTERED')
+            for key in memberToTotal:
+                print(balance_dict[key].value)
+                if balance_dict[key].value:
+                    balance_dict[key].value = float(balance_dict[key].value) + memberToTotal[key]
+                else:
+                    balance_dict[key].value = memberToTotal[key]
+                updated_cells.append(balance_dict[key])
 
+            self._balances.update_cells(balance_dict.values(), value_input_option = "USER_ENTERED")
 
+    def remove(self, initial: int, end: int):
+        for row in range(initial, end + 1):
+            self._order_record.delete_row(row)
 
-
-    def remove(self, initial: int, end:int):
-        for row in range(initial,end+1):
-            self._sheet.delete_row(row)
-        print("size=",self.getSize())
-
-    def getSize(self):
-        return len(self._data)
-
-
-
-
-
-    def get_all_sheets(self):
-        return self._sh.worksheets()
-
-    def withdraw(self, name :str, value : float):
-        cell_list= self._sh.worksheets()[1].findall(name.title())
+    def withdraw(self, name: str, value: float):
+        cell_list = self._balances.findall(name.title())
         if len(cell_list) == 0:
             print('Name does not exist')
-            return
-        row= cell_list[0].row
-        col= cell_list[0].col
-        initial_val= float(self._sh.worksheets()[1].cell( row +1 , col ).value)
-        self._sh.worksheets()[1].update_cell(row+1,col,initial_val - value )
-
-
-
-
-
+        else:
+            row = cell_list[0].row
+            col = cell_list[0].col
+            initial_val = float(self._balances.cell(row + 1, col).value)
+            self._balances.update_cell(row + 1, col, initial_val - value)
