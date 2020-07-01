243370bc62c310181aca9658480bb9ba9d0dcd60
Nishant Sinha <nishant813@gmail.com>
2020-06-30 20:58:32 -0700

    Altered button layout and revised dark theme

--- FlutterApp/divvy/lib/main.dart
+++ FlutterApp/divvy/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -12,65 +11,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Divvy',
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          primarySwatch: Colors.blue,
+          backgroundColor: Colors.black),
       home: MyHomePage(title: 'Divvy'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
+  MyHomePage({Key key, this.title}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  Future<void> _payWithVenmo(double amount) async {
-    String url =
-        'venmo://paycharge?txn=pay&recipients=ArjunMitra&amount=$amount&note=Divvy';
-    if (await canLaunch(url)) {
-      await launch(url);
-    } else {
-      throw 'Could not launch $url';
-    }
+Future<void> _payWithVenmo() async {
+  double balance = 130.95;
+  String url =
+      'venmo://paycharge?txn=pay&recipients=ArjunMitra&amount=$balance&note=Divvy';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
+}
 
+AspectRatio balanceDisplay = AspectRatio(
+    aspectRatio: 2,
+    child: Container(
+        // color: Theme.of(context).primaryColor,
+        color: Colors.black,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Balance', style: TextStyle(fontSize: 30)),
+              Text('\$130.95', style: TextStyle(fontSize: 80)),
+            ])));
+
+Container sendButton = Container(
+    width: 80,
+    height: 80,
+    child: FloatingActionButton(
+      onPressed: () => _payWithVenmo(),
+      child: Icon(
+        Icons.attach_money,
+        size: 50,
+      ),
+    ));
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     double balance = 130.95;
     return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      floatingActionButton: sendButton,
       body: ListView(
-        padding: new EdgeInsets.all(20),
         children: <Widget>[
+          Divider(color: Colors.white),
           Column(
-            children: <Widget>[
-              AspectRatio(
-                  aspectRatio: 2,
-                  child: Container(
-                      color: Colors.blue,
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text('Balance', style: TextStyle(fontSize: 30)),
-                            Padding(padding: new EdgeInsets.all(5)),
-                            Text('\$$balance', style: TextStyle(fontSize: 80)),
-                            Padding(padding: new EdgeInsets.all(20))
-                          ]))),
-              Container(
-                  width: MediaQuery.of(context).size.width,
-                  child: RaisedButton(
-                    onPressed: () => setState(() {
-                      Future<void> _launched = _payWithVenmo(balance);
-                    }),
-                    child: const Text('Pay with Venmo',
-                        style: TextStyle(fontSize: 20.0)),
-                  ))
-            ],
+            children: <Widget>[balanceDisplay],
           ),
         ],
       ),
